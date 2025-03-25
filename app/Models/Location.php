fde144c9788a0bb8c5f0e1d4498f97485d50df3f
GANZO9055 <kosta.maslihov@gmail.com>
2025-03-25 19:00:52 +0300

    feat(Location.php, Session.php, User.php): adding logic to models

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Location extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'locations';
+
+    protected $fillable = [
+        'name',
+        'latitude',
+        'longitude',
+    ];
+
+    protected function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    protected $casts = [
+        'password' => 'hashed'
+    ];
+
+    protected function locations(): HasMany
+    {
+        return $this->hasMany(Location::class);
+    }
+
+    protected function sessions(): HasMany
+    {
+        return $this->hasMany(Session::class);
+    }
 }
